14a80ac69d8bcf223449195cf2102e3b80d6aa1a
Vincent Nguyen <vqnguyen16@gmail.com>
2015-04-07 15:08:15 +0000

    fixed hello.js....hopefully

--- hello.js
+++ hello.js
@@ -1,5 +1,8 @@
 var http = require('http');
+
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World CIS 425. time to graduate\n')
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n'); // Hello WORLD!
 }).listen(process.env.PORT, process.env.IP);
+
+console.log('Server running at http://127.0.0.1:1337/');

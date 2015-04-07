c5cab1765c42e62afb537138bf56d1486a4332c8
Vincent Nguyen <vqnguyen16@gmail.com>
2015-04-07 14:58:54 +0000

    Fixed hello js it so that it actually works

--- hello.js
+++ hello.js
@@ -1,8 +1,5 @@
 var http = require('http');
-
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n'); // Hello WORLD!
-}).listen(1337, '127.0.0.1');
-
-console.log('Server running at http://127.0.0.1:1337/');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World CIS 425. time to graduate\n')
+}).listen(process.env.PORT, process.env.IP);

5a3611bb5007ed68e4b5850e6a07d2d0a7d684c6
Vincent Nguyen <vqnguyen16@gmail.com>
2015-04-07 20:58:46 +0000

    readded semicolon. make it work

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello CIS 440\n') // Hello WORLD!
+  res.end('Hello CIS 440\n'); // Hello WORLD!
 }).listen(process.env.PORT, process.env.IP);
 
 console.log('Server running at http://127.0.0.1:1337/');
